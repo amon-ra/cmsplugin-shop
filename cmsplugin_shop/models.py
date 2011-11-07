bcdfb47cf1bae39ea109092699ebba96a294a529
Zenobius Jiricek <zeno.jiricek@urpages.net>
2011-11-07 22:45:16 +1030

    include missing imports

--- cmsplugin_shop/models.py
+++ cmsplugin_shop/models.py
@@ -1,4 +1,11 @@
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+from cms.models.pluginmodel import CMSPlugin
+
+from shop.models.productmodel import Product
+
 
 class TopProducts(CMSPlugin):
     number_of_products = models.IntegerField(help_text=_('How many products to show'))
-    
+
